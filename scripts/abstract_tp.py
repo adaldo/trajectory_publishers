080a2bea454d4dd7f090393bcdfe8e8dc76dfdf2
adaldo <antonio.adaldo.89@gmail.com>
2016-11-19 19:23:24 +0100

    abstract trajectory

---
+++ scripts/abstract_tp.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+
+import rospy as rp
+import geometry_msgs.msg as gms
+
+
+class AbstractTP:
+
+    @classmethod
+    def compute_point(self, time):
+        raise NotImplementedError()
+        #return gms.Point()
+
+
+    def __init__(
+            self,
+            frequency=rp.get_param('frequency',1e1)
+            ):
+        rp.init_node('abstract_tp')
+        pub = rp.Publisher(
+            name='trajectory_point',
+            data_class=gms.Point,
+            queue_size=10)
+        RATE = rp.Rate(frequency)
+        INIT_TIME = rp.get_time()
+        while not rp.is_shutdown():
+            time = rp.get_time()-INIT_TIME
+            #rp.logwarn(time)
+            point = self.compute_point(time)
+            #rp.logwarn(point)
+            pub.publish(point)
+            RATE.sleep()
+
+
+if __name__ == '__main__':
+    tp = AbstractTP()
